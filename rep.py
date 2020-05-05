13b0496865b29c6c3483a487b2280615fcfdb8d9
sripriya41121 <64853752+sripriya41121@users.noreply.github.com>
2020-05-05 21:03:11 +0530

    First commit

---
+++ rep.py
@@ -0,0 +1,18 @@
+p1=int(input("Enter p1: "))
+p2=int(input("Enter p2: "))
+
+
+try:
+    print("Running!")
+    print(p1*p2)
+    akku=int(input("Enter your Choice: "))
+    print(akku)
+
+except ValueError as e:
+    print("Error Occured!",e)
+
+except Exception as e:
+    print("Hey, try again!")
+
+finally:
+    print("Terminated!")
